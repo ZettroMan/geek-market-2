f3c34466cc896e302d7c2d163f358d6a8552ad47
Roman Russkikh <r-ru@yandex.ru>
2020-11-23 17:05:48 +0300

    Lesson 4 homework - DONE

--- src/main/java/com/geekbrains/geek/market/services/ProductService.java
+++ src/main/java/com/geekbrains/geek/market/services/ProductService.java
@@ -3,6 +3,7 @@ package com.geekbrains.geek.market.services;
 import com.geekbrains.geek.market.entities.Product;
 import com.geekbrains.geek.market.repositories.ProductRepository;
 import com.geekbrains.geek.market.repositories.specifications.ProductSpecifications;
+import com.geekbrains.geek.market.soap.ProductSOAP;
 import com.geekbrains.geek.market.utils.ProductFilter;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,22 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private static final Function<Product, ProductSOAP> functionEntityToSoap = product -> {
+        ProductSOAP productSOAP = new ProductSOAP();
+        productSOAP.setId(product.getId());
+        productSOAP.setPrice(product.getPrice());
+        productSOAP.setTitle(product.getTitle());
+        return productSOAP;
+    };
+
+
     private final ProductRepository productRepository;
 
     public Optional<Product> findById(Long id) {
@@ -34,6 +47,10 @@ public class ProductService {
 
     public Page<Product> findAll(Specification<Product> spec, int page, int size) {
         return productRepository.findAll(spec, PageRequest.of(page, size));
+    }
+
+    public List<ProductSOAP> findAll() {
+        return productRepository.findAll().stream().map(functionEntityToSoap).collect(Collectors.toList());
     }
 
     public Product saveOrUpdate(Product product) {
